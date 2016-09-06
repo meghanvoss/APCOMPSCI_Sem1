8668819682debcfb745e7388225e016ee7bf857f
meghanvoss <meghanvoss18@gmail.com>
2016-09-06 11:00:54 -0700

    Added Files

---
+++ APLesson_01/duck.java
@@ -0,0 +1,10 @@
+public class duck
+{
+	public static void main(String[]args)
+		{
+			System.out.println("               __");
+			System.out.println("             <(o )__");
+			System.out.println("              ( ._> /");
+			System.out.println("               `---'  ");
+		}
+}

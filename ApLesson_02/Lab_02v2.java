7955b5000e486e63d56b34f1f7c6f2f7abb3fb9f
meghanvoss <meghanvoss18@gmail.com>
2016-09-08 11:33:13 -0700

    another copy of lab 2

---
+++ ApLesson_02/Lab_02v2.java
@@ -0,0 +1,26 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int x = 2;
+		int y = 4;
+		
+		System.out.println( x + " multiplied by " + y + " is " + x * y);
+		
+		String a = "Meghan Voss";
+		String b = "405 Glenmont Drive";
+		String c = "Solana Beach";
+		String d = "92075";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c + " " + d);
+		
+		int l = 3;
+		int w = 8;
+		int h = 10;
+		int surfaceArea = 2 * l * w + 2 * l * h + 2 * w * h;
+		
+		System.out.println("The surface area of your rectangle is " + surfaceArea);
+	}
+}

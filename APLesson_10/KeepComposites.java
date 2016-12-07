d27d07a2d63f992422706199f15041b538a71683
meghanvoss <VossM4374@sduhsd.lan>
2016-12-07 10:45:00 -0800

    10

---
+++ APLesson_10/KeepComposites.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class KeepComposites
+{
+	public static void main(String[]args)
+	{
+		Integer [] numbers = {2, 6, 8, 9, 10, 12, 13, 15, 17, 24, 55, 66, 78, 77, 79};
+		ArrayList<Integer> nums = new ArrayList<>(Array.asList(numbers));
+		
+		System.out.println(nums);
+	}
+	public static int gFactor(int n)
+	{
+		
+	}
+}
